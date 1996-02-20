6a9d7bebdc0e7000382ef07b7a0d56c2530df853
John A.R. Williams <J.A.R.Williams@aston.ac.uk>
1996-02-20 08:18:53 +0000

    Import of JARW/Math-VecStat-0.03 from CPAN.
    gitpan-cpan-distribution: Math-VecStat
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         JARW/Math-VecStat-0.03.tar.gz
    gitpan-cpan-author:       JARW
    gitpan-cpan-maturity:     released

--- VecStat.pm
+++ VecStat.pm
@@ -1,17 +1,27 @@
-# $Id: VecStat.pm,v 1.3 1996/01/06 11:03:30 willijar Exp $
+# $Id: VecStat.pm,v 1.4 1996/02/20 07:53:10 willijar Exp $
 =head1 NAME
 
     Math::VecStat - Some basic numeric stats on vectors
 
 =head1 SYNOPSIS
 
-    use Math::VecStat qw(max min sum average);
-    $max=max($v1,$v2,...);
+    use Math::VecStat qw(max min maxabs minabs sum average);
     $max=max(@vector);
     $max=max(\@vector);
-    $min=min($v1,$v2,...);
+    ($max,$imax)=max(@vector);
+    ($max,$imax)=max(\@vector);
     $min=min(@vector);
     $min=min(\@vector);
+    ($max,$imin)=min(@vector);
+    ($max,$imin)=min(\@vector);
+    $max=maxabs(@vector);
+    $max=maxabs(\@vector);
+    ($max,$imax)=maxabs(@vector);
+    ($max,$imax)=maxabs(\@vector);
+    $min=minabs(@vector);
+    $min=minabs(\@vector);
+    ($max,$imin)=minabs(@vector);
+    ($max,$imin)=minabs(\@vector);
     $sum=sum($v1,$v2,...);
     $sum=sum(@vector);
     $sum=sum(\@vector);
@@ -23,17 +33,35 @@
 
     This package procides some basic statistics on numerical
 vectors. All the subroutines can take a copy of the vector, or,
-preferably a reference to the vector to be operated on.
+preferably for efficiency, a reference to the vector to be operated
+on.
 
 =over 5
 
-=item  max($v1,$v2,..),  max(@vector), max(\@vector)
+=item  max(@vector), max(\@vector)
 
-return the maximum value of given values or vector
+return the maximum value of given values or vector. In an array
+context returns the value and the index in the array where it
+occurs.
 
-=item min($v1,$v2,..), min(@vector), min(\@vector)
+=item min(@vector), min(\@vector)
 
-return the minimum value of given values or vector
+return the minimum value of given values or vector, In an array
+context returns the value and the index in the array where it
+occurs.
+
+
+=item maxabs(@vector), maxabs(\@vector)
+
+return the maximum value of absolute of the given values or vector. In
+an array context returns the value and the index in the array where it
+occurs.
+
+=item minabs(@vector), minabs(\@vector)
+
+return the minimum value of the absolute of the given values or
+vector, In an array context returns the value and the index in the
+array where it occurs.
 
 =item sum($v1,$v2,...), sum(@vector), sum(\@vector)
 
@@ -46,6 +74,11 @@ return the average of the given values or vector
 =head1 HISTORY
 
  $Log: VecStat.pm,v $
+ Revision 1.4  1996/02/20 07:53:10  willijar
+ Added ability to return index in array contex to max and min
+ functions. Added minabs and maxabs functions.
+ Thanks to Mark Borges <mdb@cdc.noaa.gov> for these suggestions.
+
  Revision 1.3  1996/01/06 11:03:30  willijar
  Fixed stupid bug that crept into looping in min and max functions
 
@@ -68,23 +101,43 @@ John A.R. Williams <J.A.R.Williams@aston.ac.uk>
 require Exporter;
 package Math::VecStat;
 @ISA=qw(Exporter);
-@EXPORT_OK=qw(max min sum average);
+@EXPORT_OK=qw(max min maxabs minabs sum average);
 use strict;
 
 sub max {
   my $v=ref($_[0]) ? $_[0] : \@_;
   my $i=$#{$v};
+  my $j=$i;
   my $m=$v->[$i];
-  while (--$i >= 0) { if ($v->[$i] > $m) { $m=$v->[$i]; }}
-  return $m;
+  while (--$i >= 0) { if ($v->[$i] > $m) { $m=$v->[$i]; $j=$i; }}
+  return wantarray ? ($m,$j): $m;
 }
 
 sub min {
   my $v=ref($_[0]) ? $_[0] : \@_;
   my $i=$#{$v};
+  my $j=$i;
   my $m=$v->[$i];
-  while (--$i >= 0) { if ($v->[$i] < $m) { $m=$v->[$i]; }}
-  return $m;
+  while (--$i >= 0) { if ($v->[$i] < $m) { $m=$v->[$i]; $j=$i; }}
+  return wantarray ? ($m,$j): $m;
+}
+
+sub maxabs {
+   my $v=ref($_[0]) ? $_[0] : \@_;
+   my $i=$#{$v};
+   my $j=$i;
+   my $m=abs($v->[$i]);
+   while (--$i >= 0) { if (abs($v->[$i]) > $m) { $m=abs($v->[$i]); $j=$i}}
+   return (wantarray ? ($m,$j) : $m);
+}
+
+sub minabs {
+   my $v=ref($_[0]) ? $_[0] : \@_;
+   my $i=$#{$v};
+   my $j=$i;
+   my $m=abs($v->[$i]);
+   while (--$i >= 0) { if (abs($v->[$i]) < $m) { $m=abs($v->[$i]); $j=$i}}
+   return (wantarray ? ($m,$j) : $m);
 }
 
 sub sum {
